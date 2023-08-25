c4d52430af11f1a3bb26ff3c4a7cd6eb6444ba02
estradax <azharumuhammad@gmail.com>
2023-08-25 17:24:40 +0700

    ui: add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-import './globals.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+	<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+	  {children}
+	</ThemeProvider>
+      </body>
     </html>
   )
 }
