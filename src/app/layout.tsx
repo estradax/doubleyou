482023e7b18b8d3ea29dd0799690548b071e1a8c
estradax <azharumuhammad@gmail.com>
2023-08-26 10:48:54 +0700

    ui: add register page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
+import "./globals.css";
+
+import { Metadata } from 'next';
 import { ThemeProvider } from "@/components/theme-provider"
+
+export const metadata: Metadata = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+};
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
