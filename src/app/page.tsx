c593d7ca16533093ebe81d6372ec3bda4139077f
estradax <azharumuhammad@gmail.com>
2023-08-28 12:06:50 +0700

    ui: add QRCode page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <h1>Hello, world</h1>;
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Nav } from "./nav";
+
+export default function QRCodePage() {
+  return (
+    <>
+      <Nav />
+      <div className="flex justify-center p-4">
+	<Card className="max-w-[500px] w-full">
+	  <CardHeader>
+	    <CardTitle>QR Code</CardTitle>
+	  </CardHeader>
+	  <CardContent>
+	    <Image src="/qrcode.png" width={500} height={500} alt="" />
+	  </CardContent>
+	  <CardFooter className="flex justify-end">
+	    <Button>End session</Button>
+	  </CardFooter>
+	</Card>
+      </div>
+    </>
+  );
 }
